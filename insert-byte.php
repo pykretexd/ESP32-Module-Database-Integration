210f5c3c2c81367822eb9ffacc0fd2e2eda8f6dd
Sixten <landysixten@gmail.com>
2022-02-17 10:31:56 +0100

    Update insert-byte.php

--- insert-byte.php
+++ insert-byte.php
@@ -12,17 +12,22 @@
         printf("<p>No database connection</p>");
     }
 
+    // Function for logging
     function rx_log($message)
     {
     error_log(date('Y-m-d H:i:s') .  " : " . $message . "\n", 3, "./insert-temp.log");
     }
 
+    // Access sent packet from module
     $method = $_SERVER['REQUEST_METHOD'];
     $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
+    // If packet comes from weather station
     if ($_REQUEST['device'] == "d4") {
+        // SQL query
         $sql = sprintf("INSERT INTO public.measurements (average_wind_speed, gust_wind_speed, wind_direction, rainfall, temperature, humidity, light, uvi, station_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);");
 
+        // Parameterized query to avoid SQLi
         $result = pg_query_params($con, $sql, array($_REQUEST['average_wind_speed'],
                                                 $_REQUEST['gust_wind_speed'],
                                                 $_REQUEST['wind_direction'],
@@ -33,13 +38,15 @@
                                                 $_REQUEST['uvi'],
                                                 $_REQUEST['station_name']));
 
+        // Log results
         rx_log($sql . $_REQUEST['station_name'] . ", " .
-                  $_REQUEST['average_wind_speed'] . ", " .
-                  $_REQUEST['gust_wind_speed'] . ", " .
-                  $_REQUEST['wind_direction'] . ", " .
-                  $_REQUEST['rainfall'] . ", " .
-                  $_REQUEST['temperature'] . ", " .
-                  $_REQUEST['humidity'] . ", " .
-                  $_REQUEST['light'] . ", " .
-                  $_REQUEST['uvi'] . "(unrelated): " . $_REQUEST['device'] );  
+                      $_REQUEST['average_wind_speed'] . ", " .
+                      $_REQUEST['gust_wind_speed'] . ", " .
+                      $_REQUEST['wind_direction'] . ", " .
+                      $_REQUEST['rainfall'] . ", " .
+                      $_REQUEST['temperature'] . ", " .
+                      $_REQUEST['humidity'] . ", " .
+                      $_REQUEST['light'] . ", " .
+                      $_REQUEST['uvi'] . 
+                      "(device): " . $_REQUEST['device']);
     }
